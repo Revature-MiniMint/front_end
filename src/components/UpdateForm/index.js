32f527e4b2791d171bf694ad1819c8644ec4e588
Eric Sung <eric.sung@revature.net>
2021-12-27 11:33:18 -0500

    file update

--- src/components/UpdateForm/index.js
+++ src/components/UpdateForm/index.js
@@ -70,62 +70,56 @@ const UpdateForm = () => {
     return (
         <div className='container'>
         <form onSubmit={onSubmitHandler}>
-
-            <div className="form-group mb-3">
-                {/* enctype="multipart/form-data" */}
-                <label className="form-label" htmlFor="profilePic">Profile Image</label>
-                <input className="form-control" type="file" id="profilePic" name="profilePic" accept=".jpeg,.png" onChange={onChangeHandler}/>
-                <img src={state.profilePic} alt=""/>
+            <div className="row">
+                <div className="col">
+                    <div className="form-group mb-3">
+                        {/* enctype="multipart/form-data" */}
+                        <label className="form-label" htmlFor="profilePic">Profile Image</label>
+                        <input className="form-control" type="file" id="profilePic" name="profilePic" accept=".jpeg,.png" onChange={onChangeHandler}/>
+                        <img src={state.profilePic} alt=""/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label className="form-label" htmlFor="bio">Bio</label>
+                        <input className="form-control" type="text" id="bio" name="bio" value={state.bio} onChange={onChangeHandler}/>
+                    </div>
                 </div>
-
-            <div className="form-group mb-3">
-                <label className="form-label" htmlFor="fullname">First Name, Last Name</label>
-                <input className="form-control" type="text" id="fullname" name="fullname" value={state.fullname} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
-            </div>
-
-            <div className="form-group mb-3">
-                <label className="form-label" htmlFor="alias">Alias</label>
-                <input className="form-control" type="text" id="alias" name="alias" value={state.alias} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
-            </div>
-
-            <div className="form-group mb-3">
-                <label className="form-label" htmlFor="email">Email</label>
-                <input className="form-control" type="email" id="email" name="email" value={state.email} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
-            </div>
-
-            <div className="form-group mb-3">
-                <label className="form-label" htmlFor="username">Username</label>
-                <input className="form-control" type="text" id="username" name="username" value={state.username} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
-            </div>
-
-            <div className="form-group mb-3">
-                <label className="form-label" htmlFor="password">Password</label>
-                <input className="form-control" type="password" id="password" name="password" value={state.password} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
-            </div>
-
-            <div className="form-group mb-3">
-                <label className="form-label" htmlFor="dob">Date of Birth</label>
-                <input className="form-control" type="date" id="dob" name="dob" value={state.dob} onChange={onChangeHandler}/>
-            </div>
-
-            <div className="form-check form-check-inline">
-                {gender_male_input}
-                {/* <input className="form-check-input" type="radio" id="gender_male" name="gender" value="male" onChange={onChangeHandler}/> */}
-                <label className="form-label" htmlFor="gender_male">Male</label>
-            </div>
-
-            <div className="form-check form-check-inline">
-                {gender_female_input}
-                {/* <input className="form-check-input" type="radio" id="gender_female" name="gender" value="female" onChange={onChangeHandler}/> */}
-                <label className="form-label" htmlFor="gender_female">Female</label>
-            </div>
-
-            <div className="form-group mb-3">
-                <label className="form-label" htmlFor="bio">Bio</label>
-                <input className="form-control" type="text" id="bio" name="bio" value={state.bio} onChange={onChangeHandler}/>
-            </div>
-            <div className="text-center">
-                <button className="btn btn-primary" type="submit">Submit</button>
+                <div className="col">
+                    <div className="form-group mb-3">
+                        <label className="form-label" htmlFor="fullname">First Name, Last Name</label>
+                        <input className="form-control" type="text" id="fullname" name="fullname" value={state.fullname} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label className="form-label" htmlFor="alias">Alias</label>
+                        <input className="form-control" type="text" id="alias" name="alias" value={state.alias} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label className="form-label" htmlFor="email">Email</label>
+                        <input className="form-control" type="email" id="email" name="email" value={state.email} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label className="form-label" htmlFor="username">Username</label>
+                        <input className="form-control" type="text" id="username" name="username" value={state.username} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label className="form-label" htmlFor="password">Password</label>
+                        <input className="form-control" type="password" id="password" name="password" value={state.password} required onInvalid={onInvalidHandler} onChange={onChangeHandler}/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label className="form-label" htmlFor="dob">Date of Birth</label>
+                        <input className="form-control" type="date" id="dob" name="dob" value={state.dob} onChange={onChangeHandler}/>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        {gender_male_input}
+                        <label className="form-label" htmlFor="gender_male">Male</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        {gender_female_input}
+                        <label className="form-label" htmlFor="gender_female">Female</label>
+                    </div>
+                </div>
+                <div className="text-center">
+                    <button className="btn btn-primary" type="submit">Submit</button>
+                </div>
             </div>
         </form>
         </div>
