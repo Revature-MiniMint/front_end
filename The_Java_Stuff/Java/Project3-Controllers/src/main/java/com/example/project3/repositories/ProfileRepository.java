cd51d1e9c6daf9850c2bb8be5a6d89de14e4b99b
unknown <teimu@pm.me>
2021-12-31 11:59:53 -0500

    Preparing to merge to main

--- The_Java_Stuff/Java/Project3-Controllers/src/main/java/com/example/project3/repositories/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project3.repositories;
-
-import com.example.project3.entities.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
