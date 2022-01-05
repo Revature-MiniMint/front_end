e553cc651cba5ab4bbbc5ec47887084d084d06f3
ericsungrevature <93632639+ericsungrevature@users.noreply.github.com>
2022-01-05 16:25:46 -0500

    Update url_constants.js

--- src/url_constants.js
+++ src/url_constants.js
@@ -3,6 +3,6 @@ export const URL_PREFIX = 'https://minimintpostsserver.azurewebsites.net'; //'ht
 
 export const PROFILE_PIC_URL_PREFIX = 'https://minimint.s3.us-east-1.amazonaws.com/';
 
-export const REGISTER_LOGIN = 'http://minimintregisterloginauthorizationserver.azurewebsites.net';
+export const REGISTER_LOGIN = 'https://minimintregisterloginauthorizationserver.azurewebsites.net';
 
 export const PROFILE = 'https://minimintprofileserver.azurewebsites.net';
